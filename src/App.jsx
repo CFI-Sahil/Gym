5b150cb9f782cc42f90b97e7b079b5370c764b2b
guptasahil <guptasahil8361@gmail.com>
2025-12-03 16:43:24 +0530

    fiz import

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import React, { useEffect, useState } from 'react'
-import Navbar from './components/NavBar/Navbar'
-import Hero from './components/Banner-main/hero'
-import AboutMe from './components/About/AboutMe'
-import TrainCard from './components/Training_cards/TrainCard'
-import Feature from './components/Features/Feature'
-import Offers from './components/OurOffers/Offers'
-import Benefit from './components/Benefits/Benefit'
-import AskQuestion from './components/FAQS/AskQuestion'
-import BmiCalc from './components/BMICALCULATOR/BmiCalc'
-import MemShip from './components/MemberShip/MemShip'
-import Testtimonials from './components/Testimonial/Testtimonials'
-import Footer from './components/FooterPart/Footer'
-import Preload from './components/PreLoadingAnimation/Preload'
+import React, { useEffect, useState } from 'react';
+import Navbar from './components/NavBar/Navbar';
+import Hero from "./components/Banner-main/Hero";
+import AboutMe from './components/About/AboutMe';
+import TrainCard from './components/Training_cards/TrainCard';
+import Feature from './components/Features/Feature';
+import Offers from './components/OurOffers/Offers';
+import Benefit from './components/Benefits/Benefit';
+import AskQuestion from './components/FAQS/AskQuestion';
+import BmiCalc from './components/BMICALCULATOR/BmiCalc';
+import MemShip from './components/MemberShip/MemShip';
+import Testtimonials from './components/Testimonial/Testtimonials';
+import Footer from './components/FooterPart/Footer';
+import Preload from './components/PreLoadingAnimation/Preload';
 const App = () => {
 
   const [loading, setLoading]  = useState(true);
